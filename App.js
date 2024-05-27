b1c769c5b6c5dd5d8428cdba3ff169b1d5e0754e
Vishal Balasubramanian <vishal.bala.nps@gmail.com>
2024-05-27 22:38:29 +0530

    Implemented fetching hints from a remote server

--- App.js
+++ App.js
@@ -9,51 +9,82 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Scan from './components/scan.js'
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as ScopedStorage from "react-native-scoped-storage"
-import { ToastAndroid,Text,Alert } from 'react-native'
-import { IconButton } from 'react-native-paper';
-import RNBiometrics from "react-native-simple-biometrics";
-import {Linking,StatusBar} from 'react-native';
-import RNExitApp from 'react-native-exit-app';
-
+import { ToastAndroid,View,FlatList } from 'react-native'
+import {StatusBar} from 'react-native';
+import { Button,Card,TextInput,Text,List } from 'react-native-paper'
+import Modal from "react-native-modal";
+import axios from 'axios';
 function App() {
   const Stack = createNativeStackNavigator();
+  const txt = React.useRef()
+  const tp = React.useRef()
   function ScUI() {
     const [ld,setld] = React.useState({ld:true,cfg:{}})
-    
+    const [mod,setmod] = React.useState(false)
+    const [lst,setlst] = React.useState([])
+    function openFiles() {
+      ScopedStorage.openDocumentTree(true).then(function(r) {
+        ScopedStorage.readFile(r.uri.concat("%2Fhints.json")).then(function(f) {
+          setConfig(JSON.parse(f))
+        }).catch(function(e) {
+          ToastAndroid.show('hints.json missing!', ToastAndroid.SHORT);
+        })
+      })
+    }
+    function setConfig(loc) {
+      setld({ld:false,cfg:loc}) 
+    }
     React.useEffect(function() {
       StatusBar.setHidden(true);
-      function openFiles() {
-        ScopedStorage.openDocumentTree(true).then(function(r) {
-          ScopedStorage.readFile(r.uri.concat("%2Fhints.json")).then(function(f) {
-            AsyncStorage.setItem("@config",r.uri.concat("%2Fhints.json")).then(function() {
-              setConfig(JSON.parse(f))
-            })
-          }).catch(function(e) {
-            ToastAndroid.show('hints.json missing!', ToastAndroid.SHORT);
-            openFiles()
-          })
-        })
-      }
-      function setConfig(loc) {
-        setld({ld:false,cfg:loc}) 
-      }
-      AsyncStorage.getItem("@config").then(function(res) {
-        if (res === null) {
-          openFiles()
-        } else {
-          ScopedStorage.readFile(res).then(function(f) {
-            setConfig(JSON.parse(f))
-          }).catch(function(e) {
-            ToastAndroid.show('hints.json missing!', ToastAndroid.SHORT);
-            openFiles()
-          })
-        }
-      })
     },[])
     if (ld.ld) {
-      return <Text>Loading please wait</Text>
+      return (
+        <>
+        <Modal isVisible={mod} onBackdropPress={function() {
+          setmod(false)
+        }} useNativeDriver={true}>
+            <View>
+              <Card>
+                  <Text />
+                  <Text variant="titleLarge" style={{textAlign:"center"}}>Download Hint</Text>
+                  <Text />
+                  <TextInput label="Enter URL" onChangeText={function(r) {
+                    txt.current = r
+                  }}/>
+                  <Button onPress={function() {
+                    axios.get("http://"+txt.current).then(function(res) {
+                      tp.current = txt.current
+                      setlst(res.data)
+                    }).catch(function() {
+                      ToastAndroid.show('Failed to load list of hints!', ToastAndroid.SHORT);
+                    })
+                  }}>Fetch Hints</Button>
+                  <Text />
+                  <FlatList data={lst} renderItem={function(it) {
+                    console.log(it.item.name)
+                    return <List.Item title={it.item.name} onPress={function() {
+                      axios.get("http://"+tp.current+"/"+it.item.name).then(function(res) {
+                        setConfig(res.data)
+                      }).catch(function() {
+                        ToastAndroid.show('Failed to load hint!', ToastAndroid.SHORT);
+                      })
+                    }}/>
+                  }}/>
+              </Card>
+            </View>
+          </Modal>
+          <Text />
+          <Text variant="titleLarge" style={{textAlign:"center"}}>Welcome to the Game! Please hand over the phone to the volunteer for next steps</Text>
+          <Text />
+          <Button mode="contained" onPress={function() {
+            setmod(true)
+          }}>Download Hint</Button>
+          <Text />
+          <Button mode="contained" onPress={openFiles}>Use Stored Hint</Button>
+          <Text />
+        </>
+      )
     } else {
       return <Scan cf={ld.cfg} /> 
     }
@@ -61,43 +92,7 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Scan" component={ScUI} options={{
-          headerRight: function() {
-            return (
-              <IconButton
-                icon="cog"
-                size={20}
-                onPress={() => RNBiometrics.requestBioAuth("Enter Device Pin", "Enter Device Pin to access settings").then(function() {
-                  Alert.alert('Settings', 'Please select a setting',[
-                    {
-                      "text":"Open AppInfo",
-                      onPress:function() {
-                        Linking.openSettings()
-                        RNExitApp.exitApp()
-                      }
-                    },
-                    {
-                      "text":"Close App",
-                      onPress: function() {
-                        RNExitApp.exitApp();
-                      }
-                    },
-                    {
-                      "text":"Clear Hints and close",
-                      onPress: function() {
-                        AsyncStorage.clear().then(function() {
-                          RNExitApp.exitApp();
-                        })
-                      }
-                    }
-                  ])
-                }).catch(function(e) {
-                  ToastAndroid.show('Device either has no lock code or Auth cancel', ToastAndroid.SHORT);
-                })}
-              />
-            )
-          }
-        }} />
+        <Stack.Screen name="Scan" component={ScUI} />
       </Stack.Navigator>
     </NavigationContainer>
   )
